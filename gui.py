7a14b39d8f937d3f4648c95160b3598180de57e3
Crystal Mace <crymace@outlook.com>
2023-07-28 00:07:52 +0200

    Implement Edit Button. Improve Add functionality. Various application improvements

--- gui.py
+++ gui.py
@@ -4,9 +4,13 @@ import PySimpleGUI as gui
 label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter todo", key="todo")
 add_button = gui.Button("Add")
+list_box = gui.Listbox(values=get_todos(), key="todos",
+                       enable_events=True, size=(45, 10))
+edit_button = gui.Button("Edit")
 
 window = gui.Window("My To-Do App",
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button],
+                            [list_box, edit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -17,6 +21,20 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
 
         case gui.WIN_CLOSED:
             break
